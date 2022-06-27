4a3e91369e777fa6b502e830a4f195374e644d10
sam gardner <samgardner20@gmail.com>
2022-06-27 10:42:10 -0400

    added selectors and data-ids to use for future functions

---
+++ app.js
@@ -0,0 +1,12 @@
+const display = document.querySelector('.display')
+const a_cBtn = document.querySelector('.a-c')
+
+const equalBtn = document.querySelector('.equals')
+const addBtn = document.querySelector('.add')
+const subtractBtn = document.querySelector('.subtract')
+const divideBtn = document.querySelector('.divide')
+const multiplyBtn = document.querySelector('.multiply')
+
+const numBtns = document.querySelectorAll('.num')
+
+numBtns.forEach(num =>console.log(num.dataset.id))
